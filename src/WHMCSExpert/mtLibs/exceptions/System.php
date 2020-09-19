ef10df42f94e3ecb0b95d713ac2d67f9d675d1b0
Andre Bellafronte <andre@eunarede.com>
2020-09-19 14:34:45 -0300

    exceptions

---
+++ src/WHMCSExpert/mtLibs/exceptions/System.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WHMCSExpert\mtLibs\exceptions;
+use WHMCSExpert as main;
+
+
+/**
+ * Use for general module errors
+ *
+ * @author Michal Czech <michael@modulesgarden.com>
+ */
+class System extends Base {
+    public function __construct($message, $code = 0, $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
